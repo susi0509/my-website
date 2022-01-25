e9132212c8b033de0825878e5ae03958ae84b83c
susi0509 <98306634+susi0509@users.noreply.github.com>
2022-01-25 23:01:23 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Datalinks',
   tagline: 'Datalinks are cool',
   url: 'https://facebook.github.io/build/',
-  baseUrl: '/build/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
